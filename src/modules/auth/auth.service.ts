9a5bc3f22d7ea8b5f9d24de808aa67000c04440c
aHasnat1997 <hasnat56703@gmail.com>
2023-12-18 21:04:48 +0600

    ADD: auth controller

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,10 +1,21 @@
+import AppError from "../../errors/AppError";
+import { HTTPStatusCode } from "../../utils/httpCode";
 import { UserModel } from "../users/user.model";
 import { TAuth } from "./auth.interface";
 
 const LoginUser = async (payload: TAuth) => {
     const user = await UserModel.findOne({ id: payload.id });
+    if (!user) {
+        throw new AppError(HTTPStatusCode.NotFound, 'This user is not found!');
+    } else if (user?.isDeleted) {
+        throw new AppError(HTTPStatusCode.Forbidden, 'User is already deleted!');
+    } else if (user?.status === 'blocked') {
+        throw new AppError(HTTPStatusCode.Forbidden, 'User is blocked!');
+    }
 
-    return user;
+
+
+    return 'User Login...';
 };
 
 export const AuthService = {
