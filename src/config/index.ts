b6923c43cad5a3d31565c28d74b65f878f4a4719
aHasnat1997 <hasnat56703@gmail.com>
2023-12-19 01:19:48 +0600

    ADD: jwt

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,5 +6,9 @@ export default {
     port: process.env.PORT,
     databaseURL: process.env.DEV_DB_URL,
     bcrypt_salt: process.env.BCRYPT_SALT,
-    user_default_password: process.env.USER_DEFAULT_PASSWORD
+    user_default_password: process.env.USER_DEFAULT_PASSWORD,
+    jwt_access_secret: process.env.JWT_ACCESS_SECRET,
+    jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
+    jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
+    jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN
 };
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,7 +1,9 @@
+import jwt from "jsonwebtoken";
 import AppError from "../../errors/AppError";
 import { HTTPStatusCode } from "../../utils/httpCode";
 import { UserModel } from "../users/user.model";
 import { TAuth } from "./auth.interface";
+import config from "../../config";
 
 const LoginUser = async (payload: TAuth) => {
     const user = await UserModel.findOne({ id: payload.id });
@@ -11,12 +13,21 @@ const LoginUser = async (payload: TAuth) => {
         throw new AppError(HTTPStatusCode.Forbidden, 'User is already deleted!');
     } else if (user?.status === 'blocked') {
         throw new AppError(HTTPStatusCode.Forbidden, 'User is blocked!');
-    } else if (!(await UserModel.matchedPassword(payload.password, user.password))) {
+    } else if (!(await UserModel.matchedPassword(payload?.password, user?.password))) {
         throw new AppError(HTTPStatusCode.Forbidden, 'Wrong Password');
     }
 
+    const jwtPayload = {
+        userId: user?.id,
+        role: user?.role
+    }
+    const accessToken = jwt.sign(jwtPayload, config.jwt_access_secret as string, { expiresIn: config.jwt_access_expires_in });
+    const refreshToken = jwt.sign(jwtPayload, config.jwt_refresh_secret as string, { expiresIn: config.jwt_refresh_expires_in });
 
-    return 'User Login...';
+    return {
+        accessToken,
+        refreshToken
+    };
 };
 
 export const AuthService = {
