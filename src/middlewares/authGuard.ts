a3270475cccbd5c04b918c96c9c4e49aea8b5e88
aHasnat1997 <hasnat56703@gmail.com>
2023-12-20 02:48:01 +0600

    ADD: auth guard middlewar

---
+++ src/middlewares/authGuard.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from "express";
+import handleAsyncReq from "./handelAsyncReq";
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import config from "../config";
+import AppError from "../errors/AppError";
+import { HTTPStatusCode } from "../utils/httpCode";
+
+
+/**
+ * auth guard middlewares
+ * @param roles user access role
+ * @returns async function
+ */
+export const authGuard = (...roles: string[]) => {
+    return handleAsyncReq(async (req: Request, res: Response, next: NextFunction) => {
+        const token = req?.cookies?.accessToken;
+
+        const decoded = jwt.verify(token, config.jwt_access_secret as string) as JwtPayload;
+        if (!token || !decoded) {
+            throw new AppError(HTTPStatusCode.Unauthorized, 'Unauthorized...');
+        } else if (roles && !roles.includes(decoded?.role)) {
+            throw new AppError(HTTPStatusCode.Unauthorized, 'Role Unauthorized...');
+        }
+        console.log('test', decoded);
+
+        next();
+    });
+};
--- src/modules/students/student.route.ts
+++ src/modules/students/student.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { StudentControllers } from "./student.controller";
+import { authGuard } from "../../middlewares/authGuard";
 
 
 export const StudentRoutes = Router();
 
-StudentRoutes.get('/', StudentControllers.getAllStudent);
+StudentRoutes.get('/', authGuard('faculty'), StudentControllers.getAllStudent);
 
 StudentRoutes.get('/:userId', StudentControllers.getSingleStudent);
