af17cff0d062da21ae3f28ea1d1da2d11507a8ec
aHasnat1997 <hasnat56703@gmail.com>
2024-01-22 02:23:20 +0600

    ADD: authGuard in UserRoute

--- src/modules/users/user.route.ts
+++ src/modules/users/user.route.ts
@@ -1,16 +1,17 @@
 import { Router } from "express";
 import { UserControllers } from "./user.controller";
+import { authGuard } from "../../middlewares/authGuard";
 
 // create user route using express router
 const UserRoute = Router();
 
 // student crate route
-UserRoute.post('/students', UserControllers.createUserAsStudent);
+UserRoute.post('/students', authGuard('admin'), UserControllers.createUserAsStudent);
 
 // admin crate route
-UserRoute.post('/admins', UserControllers.createUserAsAdmin);
+UserRoute.post('/admins', authGuard('admin'), UserControllers.createUserAsAdmin);
 
 // teacher crate route
-UserRoute.post('/teachers', UserControllers.createUserAsTeacher);
+UserRoute.post('/teachers', authGuard('admin'), UserControllers.createUserAsTeacher);
 
 export default UserRoute;
