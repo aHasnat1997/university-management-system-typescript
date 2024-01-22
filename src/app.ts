ad4a4bd3434b039c6a88afc00b5d6ab3c12bb7c7
aHasnat1997 <hasnat56703@gmail.com>
2024-01-23 01:08:45 +0600

    FIX: cors

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ const app: Application = express();
 // express parsers
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(express.static('view'));
 
 // all valid routes
